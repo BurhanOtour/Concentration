8460fd605bf7237df1921d23d11335f74d74b5ff
Burhan Otour <botour@mail.uni-paderborn.de>
2020-02-23 02:42:55 +0100

    Build first UI and associated ViewController for the Concentration App

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,20 +10,40 @@ import UIKit
 // Every construct starts with UI is part of the UIKit (example: UIViewController)
 class ViewController: UIViewController {
     
-    // Keep track of the flip count
-    // var flipCount: Int
+    // Keep track of the flip count (0 is the initialization value)
+    var flipCount:Int = 0 {
+        // This block called the property observer
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    // The "!" is SUPER important
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    // An array of UIButtons / Array<UIButton>!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices: Array<String> = ["😺","👻","🎃","😺","👻","🎃"]
     
     // In swift, each argument has a name
     @IBAction func touchCard(_ sender: UIButton) {
-        flipCard(withEmoji: "👻", on: sender)
+        flipCount += 1
+        // Optional: type that has two and only two states (set, not set)
+        // For each case of an enumeration, there is an associated data
+        // It return an Optional state and has an associated value with it
+        // putting ! at the end of the optional object will return a value associated with the optional
+        // ! : is called unwrapping an optional
+        if let cardNumber = cardButtons.firstIndex(of: sender) {
+            print("card number = \(cardNumber)")
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        } else {
+            print("card is not part of the card index")
+        }
     }
-    
-    @IBAction func touchSecondCard(_ sender: UIButton) {
-        flipCard(withEmoji: "🎃", on: sender)
-    }
+
     
     func flipCard(withEmoji emoji: String, on button: UIButton) {
-        print("flipCard(withEmoji: \(emoji))")
+        // print("flipCard(withEmoji: \(   ))")
         if button.currentTitle == emoji {
             button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             button.setTitle("", for: UIControl.State.normal)
